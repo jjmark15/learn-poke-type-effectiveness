99a88f28dc57f21193d624495e310bf0310d8913
Josh Jones <ohblonddev@gmail.com>
2021-04-22 22:24:47 +0100

    add answer selection buttons

---
+++ src/lib/components/EffectivenessAnswerButton.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { Effectiveness, effectivenessString } from '$lib/effectiveness';
+
+	function handleClick() {
+		callback(effectiveness);
+	}
+
+	export let effectiveness: Effectiveness;
+	export let callback: (effectiveness: Effectiveness) => void;
+</script>
+
+<button on:click={handleClick}>{effectivenessString(effectiveness)}</button>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,22 @@
 <script lang="ts">
-	import { effectivenessString } from '$lib/effectiveness';
+	import { Effectiveness, effectivenessString } from '$lib/effectiveness';
 	import { EffectivenessCalculator } from '$lib/effectiveness-calculator';
 	import { elementString } from '$lib/element';
 	import { RandomElementSelector } from '$lib/random-element-selector';
+	import EffectivenessAnswerButton from '$lib/components/EffectivenessAnswerButton.svelte';
 
 	function generateNewElements() {
 		damageElement = elementSelector.generate();
 		defendingElement = elementSelector.generate();
+	}
+
+	function handleSelection(effectiveness: Effectiveness) {
+		effectivenessSelection = effectiveness;
+	}
+
+	function resetState() {
+		effectivenessSelection = undefined;
+		generateNewElements();
 	}
 
 	const effectivenessCalculator = new EffectivenessCalculator();
@@ -16,9 +26,18 @@
 	let defendingElement = elementSelector.generate();
 
 	$: effectivess = effectivenessCalculator.calculate(damageElement, defendingElement);
+	let effectivenessSelection: Effectiveness;
 </script>
 
 <p>Damage type is {elementString(damageElement)}</p>
 <p>Defending type is {elementString(defendingElement)}</p>
-<p>{effectivenessString(effectivess)}</p>
-<button on:click={generateNewElements}>Refresh</button>
+{#if effectivenessSelection !== undefined}
+	<p>Correct answer is {effectivenessString(effectivess)}</p>
+{/if}
+<div>
+	<EffectivenessAnswerButton effectiveness={Effectiveness.Zero} callback={handleSelection} />
+	<EffectivenessAnswerButton effectiveness={Effectiveness.Half} callback={handleSelection} />
+	<EffectivenessAnswerButton effectiveness={Effectiveness.Single} callback={handleSelection} />
+	<EffectivenessAnswerButton effectiveness={Effectiveness.Double} callback={handleSelection} />
+</div>
+<button on:click={resetState}>Next</button>
