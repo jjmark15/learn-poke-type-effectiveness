e44621f56edf0848e9e359000f6aba61fc286c01
Josh Jones <ohblonddev@gmail.com>
2021-04-24 10:24:36 +0100

    add open graph meta properties

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -1,5 +1,15 @@
 <script>
 	import '../app.postcss';
+	import { browser } from '$app/env';
 </script>
+
+<svelte:head>
+	<meta property="og:title" content="Learn Pokemon Type Effectiveness" />
+	<meta property="og:description" content="Learn Pokemon Type Effectiveness" />
+	<meta property="og:type" content="website" />
+	{#if browser}
+		<meta property="og:url" content={window.location.href} />;
+	{/if}
+</svelte:head>
 
 <slot class="h-full" />
