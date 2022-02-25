9a09928f19a092176f06dfec3b90a0314782043c
Josh Jones <ohblonddev@gmail.com>
2022-02-25 18:34:22 +0000

    Update dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,18 @@
 import preprocess from 'svelte-preprocess';
-import netlify from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: [preprocess({
-        "postcss": true
-    })],
+	preprocess: preprocess(),
 
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		adapter: netlify()
+		adapter: adapter({
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app
+			split: false
+		})
 	}
 };
 
