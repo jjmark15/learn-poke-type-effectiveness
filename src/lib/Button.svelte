cbb5bd73ad1b02cf8a78c190fe52ec09d7440995
Josh Jones <ohblonddev@gmail.com>
2022-03-01 20:24:34 +0000

    Nest button css styles

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import { createEventDispatcher } from 'svelte';
-	import { dark } from '$lib/stores';
 
 	export let disabled: boolean = false;
 	let classNames: string = '';
@@ -14,30 +13,4 @@
 	}
 </script>
 
-<button
-	class="btn {classNames}"
-	class:btn--dark={$dark}
-	class:btn--disabled-dark={$dark && disabled}
-	class:btn--disabled={disabled}
-	{disabled}
-	on:click={() => handleSelection()}
-	><slot />
-</button>
-
-<style>
-	.btn {
-		@apply py-1 px-2 rounded-lg bg-pink-100 shadow-md;
-	}
-
-	.btn--dark {
-		@apply text-gray-50 bg-gray-700;
-	}
-
-	.btn--disabled {
-		@apply text-pink-200;
-	}
-
-	.btn--disabled-dark {
-		@apply text-gray-500 bg-gray-800;
-	}
-</style>
+<button class="btn {classNames}" {disabled} on:click={() => handleSelection()}><slot /> </button>
--- src/lib/EffectivenessButton.svelte
+++ src/lib/EffectivenessButton.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { effectivenessString, type Effectiveness } from './effectiveness';
 	import { createEventDispatcher } from 'svelte';
-	import { dark } from '$lib/stores';
 
 	let isSelected: boolean;
 	export let isCorrect: boolean;
@@ -27,37 +26,8 @@
 
 <button
 	class="btn {classNames}"
-	class:btn--dark={$dark}
-	class:btn--correct={isCorrect && disabled}
-	class:btn--wrong={!isCorrect && isSelected}
-	class:btn--disabled={disabled}
-	class:btn--disabled-dark={disabled && $dark}
+	class:correct={isCorrect && disabled}
+	class:incorrect={!isCorrect && isSelected}
 	{disabled}
 	on:click={() => handleSelection()}>{effectivenessString(effectiveness)}</button
 >
-
-<style>
-	.btn {
-		@apply py-1 px-2 rounded-lg bg-pink-100 shadow-md border-2 border-transparent;
-	}
-
-	.btn--dark {
-		@apply text-gray-50 bg-gray-700;
-	}
-
-	.btn--disabled {
-		@apply text-pink-200;
-	}
-
-	.btn--disabled-dark {
-		@apply text-gray-500 bg-gray-800;
-	}
-
-	.btn--correct {
-		@apply border-green-300 text-green-300;
-	}
-
-	.btn--wrong {
-		@apply border-red-500 text-red-500;
-	}
-</style>
