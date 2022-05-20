2591f06b9c68c6c32f05464717bf6b810f3d3a07
Josh Jones <ohblonddev@gmail.com>
2022-05-20 14:13:27 +0100

    Refresh to match new sveltekit starter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-netlify';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,8 +9,13 @@ const config = {
 
 	kit: {
 		adapter: adapter({
+			// if true, will create a Netlify Edge Function rather
+			// than using standard Node-based functions
+			edge: false,
+
 			// if true, will split your app into multiple functions
-			// instead of creating a single one for the entire app
+			// instead of creating a single one for the entire app.
+			// if `edge` is true, this option cannot be used
 			split: false
 		})
 	}
