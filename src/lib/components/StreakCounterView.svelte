f3d6327f10623a0a388b16f19406e7cd4e386acb
Josh Jones <ohblonddev@gmail.com>
2022-08-08 17:17:32 +0100

    Fade high score in on load

--- src/lib/components/StreakCounterView.svelte
+++ src/lib/components/StreakCounterView.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { fade } from 'svelte/transition';
+
 	export let streak: number = 0;
 	export let bestStreak: number;
 	let classNames: string = '';
@@ -8,6 +10,6 @@
 <p class="dark:text-white {classNames}">
 	Current streak: {streak}
 	{#if bestStreak !== undefined}
-		<span class="text-pink-50 dark:text-gray-500">({bestStreak})</span>
+		<span in:fade class="text-pink-50 dark:text-gray-500">({bestStreak})</span>
 	{/if}
 </p>
