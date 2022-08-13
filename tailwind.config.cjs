5d481fcfaf069c4fa0575db65f162c0037597820
Josh Jones <ohblonddev@gmail.com>
2022-08-13 14:32:05 +0100

    Move some colours into css variables

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@ const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				"background-color": "var(--background-color)",
+				"dark-background-color": "var(--dark-background-color)",
+				"text-color": "var(--text-color)",
+				"dark-text-color": "var(--dark-text-color)",
+				"button-background-color": "var(--button-background-color)",
+				"dark-button-background-color": "var(--dark-button-background-color)",
+			}
+		}
 	},
 
 	plugins: [],
