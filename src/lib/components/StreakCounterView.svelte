5fd146777f2608c43ed11228e0005c248612617e
Josh Jones <ohblonddev@gmail.com>
2022-08-13 15:33:35 +0100

    Minor refactor

--- src/lib/components/StreakCounterView.svelte
+++ src/lib/components/StreakCounterView.svelte
@@ -7,7 +7,7 @@
 	export { classNames as class };
 </script>
 
-<p class="dark:text-white {classNames}">
+<p class={classNames}>
 	Current streak: {streak}
 	{#if bestStreak !== undefined}
 		<span in:fade class="text-pink-50 dark:text-gray-500">({bestStreak})</span>
