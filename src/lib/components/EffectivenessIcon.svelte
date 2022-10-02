e1fdb9435ad893f4b3993347f4d10d07671a919c
Josh Jones <ohblonddev@gmail.com>
2022-10-02 22:45:16 +0100

    Minor refactor

---
+++ src/lib/components/EffectivenessIcon.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { effectivenessString, type Effectiveness } from '$lib/effectiveness';
+
+	let classNames: string = '';
+	export { classNames as class };
+
+	export let effectiveness: Effectiveness;
+
+	function effectivenessClass(effectiveness: Effectiveness): string {
+		return (
+			'effectiveness--' + effectivenessString(effectiveness).toLowerCase().replaceAll(' ', '-')
+		);
+	}
+</script>
+
+<span class="{classNames} poke-icon {effectivenessClass(effectiveness)}"
+	>{effectivenessString(effectiveness)}</span
+>
+
+<style>
+	.effectiveness {
+		&--no-effect {
+			@apply bg-black text-yellow-200;
+		}
+
+		&--not-very-effective {
+			@apply bg-red-400 text-yellow-200;
+		}
+
+		&--normal {
+			@apply bg-white text-black;
+		}
+
+		&--super-effective {
+			@apply bg-green-400 text-yellow-200;
+		}
+	}
+</style>
--- src/lib/components/ScenarioWithAnswerView.svelte
+++ src/lib/components/ScenarioWithAnswerView.svelte
@@ -1,50 +1,25 @@
 <script lang="ts">
-	import { effectivenessString, type Effectiveness } from '$lib/effectiveness';
-	import ElementLabel from '$lib/components/ElementIcon.svelte';
+	import ElementIcon from '$lib/components/ElementIcon.svelte';
+	import EffectivenessIcon from './EffectivenessIcon.svelte';
 	import type { Scenario } from '$lib/scenario';
+	import type { Effectiveness } from '$lib/effectiveness';
 
 	let classNames: string = '';
 	export { classNames as class };
 
 	export let scenario: Scenario;
 	export let effectiveness: Effectiveness;
-
-	function effectivenessClass(effectiveness: Effectiveness): string {
-		return (
-			'effectiveness--' + effectivenessString(effectiveness).toLowerCase().replaceAll(' ', '-')
-		);
-	}
 </script>
 
 <div class="{classNames} text-base flex flex-row flex-wrap">
-	<ElementLabel element={scenario.offensiveElement()} />
+	<ElementIcon element={scenario.offensiveElement()} />
 	<div class="scenario-symbol">-></div>
-	<ElementLabel element={scenario.defensiveElement()} />
+	<ElementIcon element={scenario.defensiveElement()} />
 	<span class="scenario-symbol">=></span>
-	<span class="poke-icon {effectivenessClass(effectiveness)}"
-		>{effectivenessString(effectiveness)}</span
-	>
+	<EffectivenessIcon {effectiveness} />
 </div>
 
 <style>
-	.effectiveness {
-		&--no-effect {
-			@apply bg-black text-yellow-200;
-		}
-
-		&--not-very-effective {
-			@apply bg-red-400 text-yellow-200;
-		}
-
-		&--normal {
-			@apply bg-white text-black;
-		}
-
-		&--super-effective {
-			@apply bg-green-400 text-yellow-200;
-		}
-	}
-
 	.scenario-symbol {
 		@apply mx-1 self-center;
 	}
