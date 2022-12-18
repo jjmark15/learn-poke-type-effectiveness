673741496125505130957366b4c825a5d66e7db3
Josh Jones <ohblonddev@gmail.com>
2022-12-18 16:35:54 +0000

    Fix dark mode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,21 +4,22 @@ module.exports = {
 	theme: {
 		extend: {
 			colors: {
-				"primary-color": "var(--primary-color)",
-				"dark-primary-color": "var(--dark-primary-color)",
-				"secondary-color": "var(--secondary-color)",
-				"dark-secondary-color": "var(--dark-secondary-color)",
-				"background-color": "var(--background-color)",
-				"dark-background-color": "var(--dark-background-color)",
-				"text-color": "var(--text-color)",
-				"dark-text-color": "var(--dark-text-color)",
-				"button-background-color": "var(--button-background-color)",
-				"dark-button-background-color": "var(--dark-button-background-color)",
-				"correct-color": "var(--correct-color)",
-				"dark-correct-color": "var(--dark-correct-color)",
-				"incorrect-color": "var(--incorrect-color)",
+				'primary-color': 'var(--primary-color)',
+				'dark-primary-color': 'var(--dark-primary-color)',
+				'secondary-color': 'var(--secondary-color)',
+				'dark-secondary-color': 'var(--dark-secondary-color)',
+				'background-color': 'var(--background-color)',
+				'dark-background-color': 'var(--dark-background-color)',
+				'text-color': 'var(--text-color)',
+				'dark-text-color': 'var(--dark-text-color)',
+				'button-background-color': 'var(--button-background-color)',
+				'dark-button-background-color': 'var(--dark-button-background-color)',
+				'correct-color': 'var(--correct-color)',
+				'dark-correct-color': 'var(--dark-correct-color)',
+				'incorrect-color': 'var(--incorrect-color)'
 			}
 		}
 	},
 	plugins: [],
-  }
+	darkMode: 'class'
+};
